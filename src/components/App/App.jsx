9686301b523949960b3ad7d18a3641e449a9b919
Tormod Sletteboe <tormod.slettebo@gmail.com>
2022-10-29 11:59:08 -0500

    set up redux store and routes

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import axios from 'axios';
 import './App.css';
 
+
+//router imports
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
+
 function App() {
 
   return (
@@ -10,6 +14,35 @@ function App() {
         <h1 className='App-title'>Feedback!</h1>
         <h4>Don't forget it!</h4>
       </header>
+      <Router>
+        <Route path="/" exact>
+          <h1>feeling</h1>
+          <Link to='/understanding'>
+            <h1>go to understanding</h1>
+          </Link>
+        </Route>
+        <Route path="/understanding" exact>
+          <h1>understanding</h1>
+          <Link to='/support'>
+            <h1>go to support</h1>
+          </Link>
+        </Route>
+        <Route path="/support" exact>
+          <h1>support</h1>
+          <Link to='/comments'>
+            <h1>go to comments</h1>
+          </Link>
+        </Route>
+        <Route path="/comments" exact>
+          <h1>comments</h1>
+          <Link to='/review'>
+            <h1>go to review</h1>
+          </Link>
+        </Route>
+        <Route path="/review" exact>
+          <h1>review</h1>
+        </Route>
+      </Router>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,5 +4,24 @@ import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//Redux
+import { Provider } from 'react-redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
+
+
+//store shelves
+
+//store itself
+const reduxStore = createStore(
+    combineReducers({
+        //store shelves
+    }), applyMiddleware(logger)
+);
+
+
+//Connect the store to my app
+ReactDOM.render(<Provider store={reduxStore}>
+                    <App />
+               </Provider>, document.getElementById('root'));
 registerServiceWorker();
