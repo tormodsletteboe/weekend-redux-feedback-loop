a88a282d5130fec210071266f4b8ec35f9539e71
Tormod Sletteboe <tormod.slettebo@gmail.com>
2022-10-30 16:28:20 -0500

    commented ThankYou.jsx

--- src/components/ThankYou/ThankYou.jsx
+++ src/components/ThankYou/ThankYou.jsx
@@ -1,17 +1,26 @@
 import {useHistory} from 'react-router-dom';
 import {useDispatch} from 'react-redux';
+
+//material ui import
 import Button from '@mui/material/Button'
 
+//when user clicks submit on the review page, user is taken to this ThankYou page, it has a thank you message and a button.
 function ThankYou() {
 
+    //used by the browser
     const history = useHistory();
+
+    //used to reset redux store
     const dispatch = useDispatch();
 
+    //user clicked the take new survey button, handle it
     const handleOnClick = ()=>{
 
+        //reset the redux store
         dispatch({
             type:'RESET_STORE',
         })
+        //go to the front page
         history.push('/');
     };
     return (
