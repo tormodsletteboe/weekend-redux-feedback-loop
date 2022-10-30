1301f7fd98a2690521d8d1e468e16007466f72d0
Tormod Sletteboe <tormod.slettebo@gmail.com>
2022-10-30 11:47:11 -0500

    matui coming along

--- src/components/FeedBackComp/FeedBackComp.jsx
+++ src/components/FeedBackComp/FeedBackComp.jsx
@@ -8,14 +8,47 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-import FormControl from '@mui/material/FormControl';
 import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
 import Container from '@mui/material/Container';
+import MenuItem from '@mui/material/MenuItem';
+
+const ranges = [
+    {
+        value: '0',
+        label: 'Bad-0',
+    },
+    {
+        value: '1',
+        label: 'OK-1',
+    },
+    {
+        value: '2',
+        label: 'Good-2',
+    },
+    {
+        value: '3',
+        label: 'Better-3',
+    },
+    {
+        value: '4',
+        label: 'Nice-4',
+    },
+    {
+        value: '5',
+        label: 'WOW!-5',
+    },
+    {
+        value: '6',
+        label: 'DUDE!!-6',
+    },
+];
 
 function FeedBackComp({ title, labelText, inputType, pushAddress, dispatchAddr, shelfname }) {
 
-    const [feedBack, setFeedBack] = useState('0');
+    const [feedBack, setFeedBack] = useState('');
+    
+
+    
 
     const history = useHistory();
     const dispatch = useDispatch();
@@ -54,25 +87,28 @@ function FeedBackComp({ title, labelText, inputType, pushAddress, dispatchAddr, 
                         {title}
                     </Typography>
                 </CardContent>
-                <CardActions>
-                    <Button>{labelText}</Button>
-                    <FormControl>
-                        {inputType === 'text' ?
-                            <TextField onChange={handleOnChange} type={inputType} value={feedBack} placeholder='...' />
-                            : <TextField inputProps={{ inputMode: 'numeric', pattern: '^[0-6]$' }}
-                                onChange={handleOnChange}
-                                type={inputType}
-                                value={feedBack}
-                                required
-                                min={0}
-                                max={6}
-                            />}
-                        <Button variant="contained" size='small' onClick={handleOnClick} >NEXT</Button>
-                    </FormControl>
+                <CardActions sx={{ justifyContent: 'space-evenly' }}>
+
+                    {inputType === 'text' ?
+                        <TextField onChange={handleOnChange} type={inputType} value={feedBack} placeholder='leave a comment ...' />
+                        : <TextField  helperText='Pick one'
+                            onChange={handleOnChange}
+                            value={feedBack}
+                            label="Select"
+                            select
+
+                        >
+                            {ranges.map((option) => (
+                            <MenuItem key={option.value} value={option.value}>
+                                {option.label}
+                            </MenuItem>
+                        ))}
+                        </TextField>
+                        }
+                    {inputType === 'text' ? <Button variant="contained" size='large' onClick={handleOnClick} >NEXT</Button> :
+                        <Button style={{ marginBottom: 24 }} variant="contained" size='large' onClick={handleOnClick} >NEXT</Button>}
+                    
                 </CardActions>
-
-
-
             </Card>
         </Container>
     );
